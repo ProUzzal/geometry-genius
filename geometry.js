7fa59ceb762e2a77c19ee3befe2a0a03ac7fcad8
ProUzzal <uzzalahmed0001@gmail.com>
2023-10-24 23:22:23 +0600

    input validation completed

--- geometry.js
+++ geometry.js
@@ -5,20 +5,25 @@
 
 
 const displayOfCalculation = function (name, area) {
-
-
-
+    console.log(area);
+    const ol = document.querySelector('#calculaiton-area-parent');
+    ol.innerHTML += `<li>${name} - ${area} cm<sup>2</sup> <span class="p-1 text-white bg-blue-700 rounded hover:bg-blue-200 hover:text-black">Convert
+                     to M2</span><i class="cursor-pointer hover:text-red-900 fa-solid fa-trash"></i>
+                    </li>`
 
 }
 
 
 //input validation;
-const inputValidationFunc = function (first, second) {
+const inputValidation = function (first, second) {
     if (isNaN(first.value.trim()) || isNaN(second.value.trim()) || first.value.length < 1 || second.value.length < 1) {
         alert('You have must input valid number');
         first.value = '';
         second.value = '';
+        return true;
     }
+
+
 }
 
 
@@ -28,17 +33,22 @@ const inputValidationFunc = function (first, second) {
 
 const areaCalculatorOfGeometry = function (geometryName) {
     if (geometryName === 'triangle') {
-        console.log('got the funciton');
         let base = document.querySelector('.tri-b');
         let height = document.querySelector('.tri-h');
-        inputValidationFunc(base, height);
+        if (inputValidation(base, height)) {
+            console.log('undefined deticted');
+            return;
+        }
+        else {
+            console.log('-', base.value, height.value);
+            const triangleArea = .5 * base.value * height.value;
+            base.value = '';
+            height.value = '';
+            return triangleArea;
+
+        }
 
 
-
-
-
-        base.value = '';
-        height.value = '';
     }
 
 }
@@ -55,9 +65,33 @@ const areaCalculatorOfGeometry = function (geometryName) {
 
 
 //eventHandler
-
+//triangle
 document.querySelector('#triangle').addEventListener('click', (e) => {
     e.preventDefault();
     const totalArea = areaCalculatorOfGeometry('triangle');
-    displayOfCalculation('Triagle', totalArea);
+    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
+})
+//rectangle
+document.querySelector('#rectangle').addEventListener('click', (e) => {
+    e.preventDefault();
+    const totalArea = areaCalculatorOfGeometry('triangle');
+    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
+})
+//paralleogram
+document.querySelector('#triangle').addEventListener('click', (e) => {
+    e.preventDefault();
+    const totalArea = areaCalculatorOfGeometry('triangle');
+    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
+})
+//rectangle
+document.querySelector('#triangle').addEventListener('click', (e) => {
+    e.preventDefault();
+    const totalArea = areaCalculatorOfGeometry('triangle');
+    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
+})
+//rectangle
+document.querySelector('#triangle').addEventListener('click', (e) => {
+    e.preventDefault();
+    const totalArea = areaCalculatorOfGeometry('triangle');
+    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
 })
