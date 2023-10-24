9c490843ffe7b0837aae283670a0468cab90a7c0
ProUzzal <uzzalahmed0001@gmail.com>
2023-10-24 20:01:31 +0600

    Added all files in the repo

---
+++ geometry.js
@@ -0,0 +1,14 @@
+//step one: Add event handler to calculator button 
+//get input field
+//calculate retrun to handler
+//display value
+
+// Area Calculation of geometry 
+
+const areaCalculatorOfGeometry = function (geometryName) {
+    if (geometryName === 'triangle') {
+        
+        return
+    }
+
+}
