2524d743f9b82aafde07ffeb5909f638fa4de74b
ProUzzal <uzzalahmed0001@gmail.com>
2023-10-25 07:21:48 +0600

    calculation result is shown to display .

--- geometry.js
+++ geometry.js
@@ -7,7 +7,7 @@
 const displayOfCalculation = function (name, area) {
     console.log(area);
     const ol = document.querySelector('#calculaiton-area-parent');
-    ol.innerHTML += `<li>${name} - ${area} cm<sup>2</sup> <span class="p-1 text-white bg-blue-700 rounded hover:bg-blue-200 hover:text-black">Convert
+    ol.innerHTML += `<li class='m-3 my-5'>${name} - ${area} cm<sup>2</sup> <span class="p-1 text-white bg-blue-700 rounded hover:bg-blue-200 hover:text-black">Convert
                      to M2</span><i class="cursor-pointer hover:text-red-900 fa-solid fa-trash"></i>
                     </li>`
 
@@ -26,6 +26,11 @@ const inputValidation = function (first, second) {
 
 }
 
+// const paralleogramRectangleAreaCount=function(){
+
+
+//     return 
+// }
 
 
 
@@ -36,20 +41,93 @@ const areaCalculatorOfGeometry = function (geometryName) {
         let base = document.querySelector('.tri-b');
         let height = document.querySelector('.tri-h');
         if (inputValidation(base, height)) {
-            console.log('undefined deticted');
             return;
         }
         else {
-            console.log('-', base.value, height.value);
             const triangleArea = .5 * base.value * height.value;
             base.value = '';
             height.value = '';
             return triangleArea;
 
         }
-
-
     }
+    //rectangle
+    else if (geometryName === 'rectangle') {
+        let length = document.querySelector('.rec-l');
+        let width = document.querySelector('.rec-w');
+        if (inputValidation(length, width)) {
+            return;
+        }
+        else {
+            const rectangle = length.value * width.value;
+            length.value = '';
+            width.value = '';
+            return rectangle;
+
+        }
+    }
+    //paralleogram+rectangle common
+    else if (geometryName === 'parallelogram') {
+        let base = document.querySelector('.paral-b');
+        let height = document.querySelector('.paral-h');
+        if (inputValidation(base, height)) {
+            return;
+        }
+        else {
+            const parallelogram = base.value * height.value;
+            base.value = '';
+            height.value = '';
+            return parallelogram;
+
+        }
+    }
+    //rhombos+triangle,pentagon are same
+    else if (geometryName === 'rhombus') {
+        let diagonalOne = document.querySelector('.rhom-d1');
+        let diagonalTwo = document.querySelector('.rhom-d2');
+        if (inputValidation(diagonalOne, diagonalOne)) {
+            return;
+        }
+        else {
+            const rhombusArea = .5 * diagonalOne.value * diagonalTwo.value;
+            diagonalOne.value = '';
+            diagonalTwo.value = '';
+            return rhombusArea;
+
+        }
+    }
+
+    //pentagon
+    else if (geometryName === 'pentagon') {
+        let perimeter = document.querySelector('.penta-p');
+        let apothem = document.querySelector('.penta-a');
+        if (inputValidation(perimeter, apothem)) {
+            return;
+        }
+        else {
+            const pentagonArea = .5 * perimeter.value * apothem.value;
+            perimeter.value = '';
+            apothem.value = '';
+            return pentagonArea;
+
+        }
+    }
+    //ellipse
+    else if (geometryName === 'ellipse') {
+        let aAxis = document.querySelector('.ellip-a');
+        let bAxis = document.querySelector('.ellip-b');
+        if (inputValidation(aAxis, bAxis)) {
+            return;
+        }
+        else {
+            const ellipseArea = (Math.PI* aAxis.value * bAxis.value).toFixed(2);
+            aAxis.value = '';
+            bAxis.value = '';
+            return ellipseArea;
+
+        }
+    }
+
 
 }
 
@@ -74,24 +152,30 @@ document.querySelector('#triangle').addEventListener('click', (e) => {
 //rectangle
 document.querySelector('#rectangle').addEventListener('click', (e) => {
     e.preventDefault();
-    const totalArea = areaCalculatorOfGeometry('triangle');
-    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
+    const totalArea = areaCalculatorOfGeometry('rectangle');
+    if (totalArea != undefined) displayOfCalculation('Rectangle', totalArea);
 })
 //paralleogram
-document.querySelector('#triangle').addEventListener('click', (e) => {
+document.querySelector('#parallelogram').addEventListener('click', (e) => {
     e.preventDefault();
-    const totalArea = areaCalculatorOfGeometry('triangle');
-    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
+    const totalArea = areaCalculatorOfGeometry('parallelogram');
+    if (totalArea != undefined) displayOfCalculation('Paralleogram', totalArea);
 })
-//rectangle
-document.querySelector('#triangle').addEventListener('click', (e) => {
+//rhombus
+document.querySelector('#rhombus').addEventListener('click', (e) => {
     e.preventDefault();
-    const totalArea = areaCalculatorOfGeometry('triangle');
-    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
+    const totalArea = areaCalculatorOfGeometry('rhombus');
+    if (totalArea != undefined) displayOfCalculation('Rhombus', totalArea);
 })
-//rectangle
-document.querySelector('#triangle').addEventListener('click', (e) => {
+//pentagon
+document.querySelector('#pentagon').addEventListener('click', (e) => {
     e.preventDefault();
-    const totalArea = areaCalculatorOfGeometry('triangle');
-    if (totalArea != undefined) displayOfCalculation('Triagle', totalArea);
+    const totalArea = areaCalculatorOfGeometry('pentagon');
+    if (totalArea != undefined) displayOfCalculation('Pentagon', totalArea);
+})
+//ellipse
+document.querySelector('#ellipse').addEventListener('click', (e) => {
+    e.preventDefault();
+    const totalArea = areaCalculatorOfGeometry('ellipse');
+    if (totalArea != undefined) displayOfCalculation('Ellipse', totalArea);
 })
